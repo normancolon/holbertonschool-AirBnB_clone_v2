11f3b6a4f8f761cd175b3f7054fcdc23d1839954
Felipe Santos <felipecassiors@gmail.com>
2024-03-21 14:10:37 -0400

    console.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
+import uuid
 from uuid import uuid4
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from models import storage
 
 Base = declarative_base()
 
@@ -19,25 +20,46 @@ class BaseModel:
         for key, value in kwargs.items():
             if key != '__class__':
                 setattr(self, key, value)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.utcnow()
+                self.updated_at = datetime.utcnow()
+                if 'created_at' in kwargs:
+                    kwargs['created_at'] = datetime.strptime(
+                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+                if 'updated_at' in kwargs:
+                    kwargs['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+                self.__dict__.update(kwargs)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls_name = type(self).__name__
+        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
 
     def save(self):
+        from models import storage
         self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
     def delete(self):
+        from models import storage
         storage.delete(self)
 
     def to_dict(self):
-        dictionary = {**self.__dict__}
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = dictionary['created_at'].isoformat()
-        dictionary['updated_at'] = dictionary['updated_at'].isoformat()
+        """Convert instance into dict format"""
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = type(self).__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     @classmethod
     def close(cls):
         """Calls remove() method(if DBStorage)."""
-        from models import storage  # Import moved inside the method
+        from models import storage
         storage.close()
