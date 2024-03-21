f99f7ec644014b36aa301152a14087db769c8849
Felipe Santos <felipecassiors@gmail.com>
2024-03-21 15:02:33 -0400

    CONSOLE.PY

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,12 @@ depending on the environment variable HBNB_TYPE_STORAGE.
 """
 
 import os
-
 # Import the declarative base for SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
-# Determine storage type based on env variable
 HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE', 'file')
 
 if HBNB_TYPE_STORAGE == 'db':
@@ -22,5 +21,16 @@ else:
     # FileStorage specific imports
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+try:
+    if HBNB_TYPE_STORAGE == 'db':
+        from sqlalchemy.ext.declarative import declarative_base
+        Base = declarative_base()
+        from models.engine.db_storage import DBStorage
+        storage = DBStorage()
+    else:
+        from models.engine.file_storage import FileStorage
+        storage = FileStorage()
+except ImportError as e:
+    print(f"Error importing storage module: {e}")
 
     storage.reload()
