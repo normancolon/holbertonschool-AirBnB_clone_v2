5074930329da8c634407d94e4512c62e53bd3b14
Felipe Santos <felipecassiors@gmail.com>
2024-03-21 22:36:10 -0400

    console.py

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,30 +1,20 @@
 -- Initialize MySQL for HBNB Clone Project Testing Environment
--- This script performs the following actions:
---   * Establishes a test database named `hbnb_clone_test`.
---   * Sets up a dedicated test user `test_user` with a specified password for accessing the test database.
---   * Ensures `test_user` has comprehensive privileges over the `hbnb_clone_test` database for full testing capabilities.
---   * Allows `test_user` to execute SELECT queries on `performance_schema` for database performance insights.
+-- Ensures a test database named `hbnb_test_db`.
+-- Creates a user `hbnb_test` with a specified password for accessing the test database.
+-- Grants `hbnb_test` full privileges over the `hbnb_test_db` database for testing.
+-- Grants `hbnb_test` SELECT privileges on `performance_schema` for database performance insights.
 
 -- Step 1: Create the test database if it doesn't already exist
-CREATE DATABASE IF NOT EXISTS hbnb_clone_test;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Step 2: Create a test user with a specific password for localhost access
-CREATE USER
-    IF NOT EXISTS 'test_user'@'localhost'
-    IDENTIFIED BY 'secure_test_pwd';
+-- Step 2: Create the test user with a specific password for localhost access
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Step 3: Grant full privileges to the test user on the test database
--- This enables the user to perform all CRUD operations necessary for thorough testing
-GRANT ALL PRIVILEGES
-   ON `hbnb_clone_test`.*
-   TO 'test_user'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 
 -- Step 4: Grant the test user permission to access `performance_schema`
--- Access to performance metrics can aid in identifying potential bottlenecks
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'test_user'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
 -- Step 5: Apply all the privilege changes
 FLUSH PRIVILEGES;
-
