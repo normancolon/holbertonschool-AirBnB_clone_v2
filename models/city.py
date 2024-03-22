b445a0854baa9f16fe1995b01e3e3236c1d35479
Felipe Santos <felipecassiors@gmail.com>
2024-03-21 22:32:50 -0400

    console.py

--- models/city.py
+++ models/city.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """City Module for HBNB project"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class City(BaseModel, Base):
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    id = Column(Integer, primary_key=True)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        places = relationship('Place', backref='cities', cascade='all, delete')
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+    places = relationship("Place", backref="city")
