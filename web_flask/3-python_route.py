1a1c360677748469188e0d7a7d5f7f3cf987438a
Felipe Santos <felipecassiors@gmail.com>
2024-04-12 15:32:08 -0400

    2-c_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Initializes a Flask web server that is accessible on 0.0.0.0 and listens on port 5000.
-This server handles various routes to display corresponding messages.
+Initializes a Flask web server that is accessible
 """
 
 from flask import Flask
@@ -22,23 +21,13 @@ def greet():
 
 @app.route("/hbnb", strict_slashes=False)
 def display_hbnb():
-    """
-    Return the string 'HBNB' when accessing the /hbnb route.
-    Returns:
-        str: 'HBNB'
-    """
+
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def show_c_text(text):
-    """
-    Return 'C ' followed by the user input with underscores replaced by spaces.
-    Args:
-        text (str): The path variable provided by the user.
-    Returns:
-        str: Formatted string with 'C ' prefix.
-    """
+
     formatted_text = text.replace("_", " ")
     return f"C {formatted_text}"
 
@@ -46,18 +35,10 @@ def show_c_text(text):
 @app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def show_python_text(text):
-    """
-    Display 'Python' followed by the provided text, with a default if none is provided.
-    Underscores in the text are replaced with spaces.
-    Args:
-        text (str): The text to display after 'Python'.
-    Returns:
-        str: Complete greeting with Python.
-    """
+
     formatted_text = text.replace("_", " ")
     return f"Python {formatted_text}"
 
 
-# Condition to run the Flask app only if this script is executed directly
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
