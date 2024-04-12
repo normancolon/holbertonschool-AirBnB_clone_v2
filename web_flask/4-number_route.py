b888531748407d0a73d2f36b8eea364824df51fc
Felipe Santos <felipecassiors@gmail.com>
2024-04-12 15:51:23 -0400

    console.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def index():
+
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_page():
+
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def show_c(text):
+
     return f"C {text.replace('_', ' ')}"
 
 
 @app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def show_python(text):
+
     return f"Python {text.replace('_', ' ')}"
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number_check(n):
+
     return f"{n} is a number"
 
 
