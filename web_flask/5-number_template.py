65cfe73d2ac0640341570bb481004c51ebe2a8b5
Felipe Santos <felipecassiors@gmail.com>
2024-04-12 15:41:03 -0400

    console.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
 from flask import Flask, render_template
 
+# Flask app setup
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def greet():
+    """Greet at root"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
+    """Show 'HBNB'"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def show_c(text):
-    formatted_text = text.replace('_', ' ')
-    return f'C {formatted_text}'
+def show_c(text="is cool"):
+    """Display 'C <text>'"""
+    return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def show_python(text):
-    formatted_text = text.replace('_', ' ')
-    return f'Python {formatted_text}'
+    """Display 'Python <text>'"""
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<n>', strict_slashes=False)
 def show_number(n):
-    return f'{n} is a number'
+    """Validate number"""
+    if n.isdigit():
+        return f'{n} is a number'
+    return '404 not found'
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', number=n)
-
-
+# Run the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
